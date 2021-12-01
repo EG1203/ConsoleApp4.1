fd09637421dc10dc567568d86b28a5ebb89876eb
МАКСИМ МАКСИМ <m12.12.2003@outlook.com>
2021-12-01 15:32:44 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp4.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[19];
+            //Fill the array here as you'd like to
+            if (a.Where(x => x < 0).Count() == 0)
+                Console.WriteLine("No negatives");
+            else
+                Console.WriteLine(a.TakeWhile(x => x >= 0).Sum());
+        }
+    }
+}
